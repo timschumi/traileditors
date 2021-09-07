04148c6d358dd7873999c2bd51d0572d57693450
Tim Schumacher <timschumi@gmx.de>
2021-09-07 20:58:41 +0200

    Also parse byte arrays that are inside player saves

--- parsesave.py
+++ parsesave.py
@@ -12,3 +12,13 @@ player_save = trailmakers_pb2.TrailmakersPlayerSaveData()
 player_save.ParseFromString(data)
 print(f"[*] TrailmakersPlayerSaveData dump:")
 print(text_format.MessageToString(player_save, indent=4))
+
+latest_structure_ident = trailmakers_pb2.StructureSaveIdentifierProto()
+latest_structure_ident.ParseFromString(player_save.m_latestStructureIdentifierBytes)
+print(f"[*] m_latestStructureIdentifierBytes dump:")
+print(text_format.MessageToString(latest_structure_ident, indent=4))
+
+latest_structure_data = trailmakers_pb2.StructureGraphSaveDataProto()
+latest_structure_data.ParseFromString(player_save.m_latestStructureData)
+print(f"[*] m_latestStructureData dump:")
+print(text_format.MessageToString(latest_structure_data, indent=4))
